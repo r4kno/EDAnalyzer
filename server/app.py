17f799eca2672e21adc98ee78d35723fdf354d36
Onkar Gupta <onkargupta0864@gmail.com>
2025-08-10 21:51:56 +0530

    deployment

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from eda import load_data_from_url, perform_eda_with_visualizations
 from dotenv import load_dotenv
+import os
 
 load_dotenv()  # Load environment variables
 
@@ -58,5 +59,7 @@ def process_file():
         print(f"Error: {str(e)}")
         return jsonify({"error": f"Processing failed: {str(e)}"}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
